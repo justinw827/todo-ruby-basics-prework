bf7b08476b099a19590345b294d4e35134165699
Justin Wang <justinw827@aol.com>
2018-07-15 01:26:14 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+  return num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(arg1)
+  return arg1
 end
 
-def greeting
+def greeting(arg1, arg2)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
